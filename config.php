a297d0e4ffe844374841f589fd84e79a0bb4c7c3
Tavon Seesenpila <thekaroe@hotmail.com>
2012-08-17 17:55:19 +0700

    เพิ่มไฟล์มาอีกแล้วนะ

---
+++ config.php
@@ -0,0 +1,37 @@
+<?php
+	$host="localhost";
+	$user="pingpongso_user";
+	$pass="12341234";
+	$db_name="pingpongso_db";
+	$conn = @mysql_connect($host,$user,$pass);
+
+	$align_web = "center";
+	if(!$conn){
+		echo("no connection database");
+		exit();
+	}else{
+		mysql_select_db($db_name);
+		mysql_query("SET NAMES utf8");
+	}
+	
+	$path_web = "http://www.pingpongsoft.com/";
+	$save = "Save Data";
+	$cancel = "Cancel";
+	$yessave = "Save Complete";
+	$nosave = "Save Fail !!";
+	$return_confirm = "Are you sure Delete?";
+        
+        function getKeywords() {
+            return array(
+                "โปรแกรมร้านคอมพิวเตอร์", 
+                "โปรแกรมร้านมินิมาร์ท", 
+                "โปรแกรมยิงบาโค้ด", 
+                "โปรแกรมเช่ารถ", 
+                "โปรแกรมดูแลนักเรียน", 
+                "โปรแกรมห้องสมุด", 
+                "โปรแกรมดูแลลูกค้า", 
+                "โปรแกรมร้านซ่อมคอม", 
+                "โปรแกรมขายปลีก ขายส่ง", 
+                "โปรแกรม POS");
+        }
+?>
